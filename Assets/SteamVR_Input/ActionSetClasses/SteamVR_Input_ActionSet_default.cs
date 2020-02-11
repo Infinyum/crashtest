0bc4989c2eb4a6b0b5656170827a7c9f3f680301
T4ouf <thomasvonascheberg@hotmail.fr>
2020-02-11 10:30:49 +0100

    Added new interactable scripts

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Plant
+        {
+            get
+            {
+                return SteamVR_Actions.default_Plant;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
