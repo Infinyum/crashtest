35f5a1d04b58dbd50c5dc2ded31f00f42a61a25b
T4ouf <thomasvonascheberg@hotmail.fr>
2020-02-26 09:25:05 +0100

    Forgotten assets in previous commit

---
+++ Assets/Scripts/deformationCollision.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//We need a mesh collider to provide mesh deformation
+[RequireComponent(typeof(MeshCollider))]
+/**
+ * deformationCollision script:
+ * Script that deforms locally a mesh after a collision
+ */
+public class deformationCollision : MonoBehaviour{
+
+	//local area aroud the collision point were deformation occurs
+	public float minDist = 1.0f;
+
+	public bool enableScript = true;	//is the script enabled
+
+	//small clamp function
+	private static T Clamp<T>(T aValue, T aMin, T aMax) where T : IComparable<T>{
+
+		var _Result = aValue;
+		if (aValue.CompareTo(aMax) > 0)
+			_Result = aMax;
+		else if (aValue.CompareTo(aMin) < 0)
+			_Result = aMin;
+		return _Result;
+	}
+
+
+	// Start is called before the first frame update
+	void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+
+    void OnCollisionEnter(Collision collisionInfo){
+
+        // Get instantiated mesh
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        MeshCollider mc = GetComponent<MeshCollider>();
+
+        // get all the vertices
+        Vector3[] vertices = mesh.vertices;
+
+		//for each contact point
+        foreach (var c in collisionInfo.contacts){
+
+            var impact = mc.ClosestPoint(c.point);
+
+			//we look for the closest vertices
+			int p = 0;
+			while (p < vertices.Length){
+
+				//We get (into world coordinates) the current vertice
+				Vector3 v = transform.TransformPoint(vertices[p]);
+
+				//We compute the distance from the impact point to the vertex
+				float dist = (impact - v).sqrMagnitude;
+
+
+				//Collision deforms a zone around the collision (not only one point)
+				//We check if we are in the "deformation" zone
+				if (dist <= minDist){
+
+					//We clamp the distance (we don't want division by 0 or very small distances)
+					dist = Clamp<float>(dist, 0.01f, minDist);
+
+					Vector3 offset = -c.normal;	//We compute the vertex offset
+					offset.Scale(collisionInfo.relativeVelocity/1000.0f);	//we must scale the offset
+					vertices[p] += offset /dist*100;
+				}
+				p++;
+			}
+
+			
+
+		}
+         
+		//we refresh the vertices and the normals
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/phantomBalistic.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Phantom script :
+ * This script is showing "phantoms" of previous position of an object. That way, we can find the whole balistic history of an object
+ */
+public class phantomBalistic : MonoBehaviour{
+
+	//We need a frame counter (as we don't show phantoms on each frame)
+    private int frameNumber;
+	
+	//////////////////// Parameters of the script ////////////////////
+
+	public Material phantomMat;			//The material we use to represent "phantoms"
+    public bool enableScript = true;    //is the script enabled ?
+	public bool drawOnCollision = false;//do we draw a phantom when the collision occurs ?
+	public int frameFrequency = 15;		//how often do we draw a phantom ? (every 15 frames currently => 0.25s)
+
+	private void drawPhantom(){
+		// Get instantiated mesh
+		Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+
+		//copy the mesh
+		Mesh mesh2 = Instantiate(mesh);
+
+		//We create a new game object that we are going to parameterize correctly 
+		GameObject obj = new GameObject();
+
+		MeshRenderer mr = obj.AddComponent<MeshRenderer>();
+		MeshFilter mf = obj.AddComponent<MeshFilter>();
+		mf.mesh = mesh2;
+
+		Material resMat = new Material(phantomMat);
+		//resMat.color = new Color(rb.velocity.x, rb.velocity.y, rb.velocity.z, 30);
+
+		obj.transform.position = transform.position;
+		obj.transform.rotation = transform.rotation;
+		obj.transform.localScale = transform.lossyScale;
+
+		obj.GetComponent<MeshRenderer>().material = resMat;
+		/*If somehow the new object is having collision detection enabled : 
+		obj.GetComponent<BoxCollider>().enabled = false;
+		obj.GetComponent<Rigidbody>().detectCollisions = false;
+		*/
+	}
+
+	// Start is called before the first frame update
+	void Start(){
+
+		//init the frame counter
+        frameNumber = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+		//if the script is enabled => we compute things
+        if (enableScript){
+            if (frameNumber % frameFrequency == 0){
+
+				drawPhantom();
+                
+            }
+
+            frameNumber++;
+        }
+        
+    }
+
+	//TODO : Add an event to enable the script (and then put default value to false)
+
+    void OnCollisionEnter(Collision collisionInfo){
+
+		if (enableScript) { 
+			//The script disable itself when a collision occurs
+			enableScript = false;
+
+			if (drawOnCollision){
+
+				//we draw a last phantom
+				drawPhantom();
+			}
+		}
+	}
+}
