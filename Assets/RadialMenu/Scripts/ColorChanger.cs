a6f25684a80dbe599ba0c20b94a8acde2f68252c
MurphyParadise <45964157+MurphyParadise@users.noreply.github.com>
2020-03-06 19:07:51 +0100

    Add Radial Menu

---
+++ Assets/RadialMenu/Scripts/ColorChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material pinkMaterial;
+    public Material greyMaterial;
+
+    public void SetPink()
+    {
+        SetMaterial(pinkMaterial);
+    }
+
+    public void SetGrey()
+    {
+        SetMaterial(greyMaterial);
+    }
+
+    private void SetMaterial(Material newMaterial)
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.material = newMaterial;
+    }
+}
---
+++ Assets/RadialMenu/Scripts/ScriptChanger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CollisionVisual;
+
+public class ScriptChanger : MonoBehaviour
+{
+
+    private CollisionVisual.collisionVisual ascript;
+    /*public DeformationCollision.deformationCollision bscript;
+    public PhantomBalistic.phantomBalistic cscript;*/
+
+    public void SetCollisionVisual()
+    {
+        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        /*bscript = GetComponent<DeformationCollision.deformationCollision>();
+        cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
+        ascript.enabled = true;
+        /*bscript.enabled = false;
+        cscript.enabled = false;*/
+    }
+
+    public void SetDeformationCollision()
+    {
+        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        /*bscript = GetComponent<DeformationCollision.deformationCollision>();
+        cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
+        ascript.enabled = false;
+        /*bscript.enabled = true;
+        cscript.enabled = false;*/
+    }
+
+    public void SetPhantomBalistic()
+    {
+        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        /*bscript = GetComponent<DeformationCollision.deformationCollision>();
+        cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
+        ascript.enabled = false;
+        /*bscript.enabled = false;
+        cscript.enabled = true;*/
+    }
+
+    public void SetNone()
+    {
+        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        /*bscript = GetComponent<DeformationCollision.deformationCollision>();
+        cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
+        ascript.enabled = false;
+        /*bscript.enabled = false;
+        cscript.enabled = false;*/
+    }
+
+
+}
