8619603124128f6789c7c1eebe278394d140ce20
MurphyParadise <45964157+MurphyParadise@users.noreply.github.com>
2020-03-06 23:16:08 +0100

    few adaptation to make it work without any problem

--- Assets/RadialMenu/Scripts/ScriptChanger.cs
+++ Assets/RadialMenu/Scripts/ScriptChanger.cs
@@ -1,51 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CollisionVisual;
+// add the namespace into each files of filters and use them here
+//using CollisionVisual;
 
 public class ScriptChanger : MonoBehaviour
 {
 
-    private CollisionVisual.collisionVisual ascript;
+    //private CollisionVisual.collisionVisual ascript;
     /*public DeformationCollision.deformationCollision bscript;
     public PhantomBalistic.phantomBalistic cscript;*/
 
     public void SetCollisionVisual()
     {
-        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        //ascript = GetComponent<CollisionVisual.collisionVisual>();
         /*bscript = GetComponent<DeformationCollision.deformationCollision>();
         cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
-        ascript.enabled = true;
+        //ascript.enabled = true;
         /*bscript.enabled = false;
         cscript.enabled = false;*/
     }
 
     public void SetDeformationCollision()
     {
-        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        //ascript = GetComponent<CollisionVisual.collisionVisual>();
         /*bscript = GetComponent<DeformationCollision.deformationCollision>();
         cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
-        ascript.enabled = false;
+        //ascript.enabled = false;
         /*bscript.enabled = true;
         cscript.enabled = false;*/
     }
 
     public void SetPhantomBalistic()
     {
-        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        //ascript = GetComponent<CollisionVisual.collisionVisual>();
         /*bscript = GetComponent<DeformationCollision.deformationCollision>();
         cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
-        ascript.enabled = false;
+        //ascript.enabled = false;
         /*bscript.enabled = false;
         cscript.enabled = true;*/
     }
 
     public void SetNone()
     {
-        ascript = GetComponent<CollisionVisual.collisionVisual>();
+        //ascript = GetComponent<CollisionVisual.collisionVisual>();
         /*bscript = GetComponent<DeformationCollision.deformationCollision>();
         cscript = GetComponent<PhantomBalistic.phantomBalistic>();*/
-        ascript.enabled = false;
+        //ascript.enabled = false;
         /*bscript.enabled = false;
         cscript.enabled = false;*/
     }
